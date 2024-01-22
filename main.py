022b8b68c0f455e3fcf1f7bcf1336ce6ac359861
ATPJ <amirali.porhonar@gmail.com>
2024-01-22 20:37:36 +0330

    Add time slice argument.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from typer import Typer
-from enum import Enum
+import typer
+
+from typing_extensions import Annotated
+from typing import Optional
 
 from models import Algo, Process
-
 from ScheduleAlgo.Scheduler import Scheduler
 
-app = Typer()
+app = typer.Typer()
 
 
 @app.command()
-def main(number: int, algo: Algo):
+def main(number: int, algo: Algo, time_slice: Annotated[Optional[int], typer.Argument()] = 10):
     # print(f'Number: {number}, algo: {algo.value}')
     processes = []
     print("Give the processes information with this format: AT CBT\nex: 0 5")
@@ -18,7 +19,7 @@ def main(number: int, algo: Algo):
         p = Process(i, at_i, cbt_i)
         processes.append(p)
 
-    sch = Scheduler(algo, processes)
+    sch = Scheduler(algo, processes, qunt=time_slice)
     sch.calculate()
 
 
